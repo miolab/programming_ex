845306b9fd9336c6ae02285dc99972a8f0c864ef
im <poson_hemp@yahoo.co.jp>
2020-07-04 20:56:28 +0900

    add function: def triple

--- ch_6/times.ex
+++ ch_6/times.ex
@@ -1,15 +1,23 @@
 defmodule Times do
   @moduledoc """
-  入力数値を2倍する関数 `double/1` を提供する
+  入力数値を掛け算する関数 `double/1` を提供する
 
   ## e.g
     $ iex times.ex
-    iex(1)> Times.double 1
+    iex> Times.double 1
     2
-    iex(2)> Times.double 10
+    iex> Times.double(10)
     20
+    iex> Times.triple 1
+    3
+    iex> Times.triple(100)
+    300
   """
   def double(n) do
     n * 2
+  end
+
+  def triple(n) do
+    n * 3
   end
 end
