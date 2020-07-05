b59f1216c55cfa236c5bd936dba36fdea0af7d68
im <poson_hemp@yahoo.co.jp>
2020-07-05 16:12:19 +0900

    refactor

--- ch_6/times.ex
+++ ch_6/times.ex
@@ -13,11 +13,6 @@ defmodule Times do
     iex> Times.triple(100)
     300
   """
-  def double(n) do
-    n * 2
-  end
-
-  def triple(n) do
-    n * 3
-  end
+  def double(n), do: n * 2
+  def triple(n), do: n * 3
 end
