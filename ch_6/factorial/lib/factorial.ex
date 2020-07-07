9675fe70883a4b4aa30defa307942992ede0b67e
im <poson_hemp@yahoo.co.jp>
2020-07-07 19:12:58 +0900

    init

---
+++ ch_6/factorial/lib/factorial.ex
@@ -0,0 +1,18 @@
+defmodule Factorial do
+  @moduledoc """
+  Documentation for `Factorial`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Factorial.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
