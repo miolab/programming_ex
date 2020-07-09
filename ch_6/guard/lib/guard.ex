97e41afebf518893e0d0e5acb3e5a407e5e15ba4
im <poson_hemp@yahoo.co.jp>
2020-07-09 19:51:32 +0900

    add tests and guard functions

--- ch_6/guard/lib/guard.ex
+++ ch_6/guard/lib/guard.ex
@@ -1,24 +1,32 @@
 defmodule Guard do
   @moduledoc """
-  Documentation for `Guard`.
+  ガード節で型判別を行う関数を提供する。
   """
 
   @doc """
-  Hello world.
+  Function Usage:
 
   ## Examples
 
-      iex> Guard.hello()
-      :world
       iex> Guard.what_is(100)
       "100 is a Number."
 
-  """
-  def hello do
-    :world
-  end
+      iex> Guard.what_is("im")
+      "im is a Binary."
 
+      iex> Guard.what_is(:miolab)
+      "miolab is a Atom."
+
+  """
   def what_is(x) when is_number(x) do
     IO.inspect("#{x} is a Number.")
+  end
+
+  def what_is(x) when is_binary(x) do
+    IO.inspect("#{x} is a Binary.")
+  end
+
+  def what_is(x) when is_atom(x) do
+    IO.inspect("#{x} is a Atom.")
   end
 end
--- ch_6/guard/test/guard_test.exs
+++ ch_6/guard/test/guard_test.exs
@@ -2,7 +2,15 @@ defmodule GuardTest do
   use ExUnit.Case
   doctest Guard
 
-  test "greets the world" do
-    assert Guard.hello() == :world
+  test "expected is_number case" do
+    assert Guard.what_is(100) == "100 is a Number."
+  end
+
+  test "expected is_binary case" do
+    assert Guard.what_is("im") == "im is a Binary."
+  end
+
+  test "expected is_atom case" do
+    assert Guard.what_is(:miolab) == "miolab is a Atom."
   end
 end
