fc393ebade7533319f49e9b916b55a12db55c415
im <poson_hemp@yahoo.co.jp>
2020-07-10 19:54:18 +0900

    add: functions and 1st DocTest

--- ch_6/default_param/lib/default_param.ex
+++ ch_6/default_param/lib/default_param.ex
@@ -11,8 +11,19 @@ defmodule DefaultParam do
       iex> DefaultParam.hello()
       :world
 
+      iex> DefaultParam.incomplete(1, 3)
+      [1, 2, 3]
+
   """
   def hello do
     :world
+  end
+
+  def filled(p1 \\ 1, p2 \\ 2, p3 \\ 3) do
+    IO.inspect([p1, p2, p3])
+  end
+
+  def incomplete(p1, p2 \\ 2, p3) do
+    IO.inspect([p1, p2, p3])
   end
 end
