f460d390fa308e9d36a188207a6260c130fc267c
im <poson_hemp@yahoo.co.jp>
2020-07-11 09:21:51 +0900

    add tests

--- ch_6/default_param/test/default_param_test.exs
+++ ch_6/default_param/test/default_param_test.exs
@@ -2,8 +2,29 @@ defmodule DefaultParamTest do
   use ExUnit.Case
   doctest DefaultParam
 
-  # DefaultParam.filled: init set [1, 2, 3]
-  test "if params to be not given" do
+  # DefaultParam.filled: default params set [1, 2, 3]
+  test "all params to be given" do
+    assert DefaultParam.filled(10, 20, 30) == [10, 20, 30]
+  end
+
+  test "two params to be given" do
+    assert DefaultParam.filled(10, 20) == [10, 20, 3]
+  end
+
+  test "one params to be given" do
+    assert DefaultParam.filled(10) == [10, 2, 3]
+  end
+
+  test "params to be NOT given" do
     assert DefaultParam.filled() == [1, 2, 3]
+  end
+
+  # DefaultParam.incomplete: default params set [_, 2, _]
+  test "next func: all params to be given" do
+    assert DefaultParam.incomplete(10, 20, 30) == [10, 20, 30]
+  end
+
+  test "next func: two params to be given" do
+    assert DefaultParam.incomplete(10, 20) == [10, 2, 20]
   end
 end
