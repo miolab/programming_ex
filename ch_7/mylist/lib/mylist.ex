4154e1ea8c1e74e4afdcd3390d65f9b2c8593627
im <poson_hemp@yahoo.co.jp>
2020-07-13 09:27:52 +0900

    add function and tests: square each param of list

--- ch_7/mylist/lib/mylist.ex
+++ ch_7/mylist/lib/mylist.ex
@@ -14,8 +14,18 @@ defmodule Mylist do
       iex> Mylist.len(["a", "b", "c"])
       3
 
+      # リストの各要素を2条する
+      iex> Mylist.square([])
+      []
+      iex> Mylist.square([1, 2, 3])
+      [1, 4, 9]
+
   """
   # リストの長さを求める
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail)
+
+  # リストの各要素を2条する
+  def square([]), do: []
+  def square([head | tail]), do: [head * head | square(tail)]
 end
--- ch_7/mylist/test/mylist_test.exs
+++ ch_7/mylist/test/mylist_test.exs
@@ -3,7 +3,16 @@ defmodule MylistTest do
   doctest Mylist
 
   test "function: count length of list" do
-    assert Mylist.len([]) == 0
-    assert Mylist.len(["a", "b", "c"]) == 3
+    list = ["a", "b", "c"]
+    list_a_len = list |> Mylist.len()
+    list_b_len = list |> length
+    assert list_a_len == list_b_len
+  end
+
+  test "function: square each param of list" do
+    list = [1, 2, 3]
+    list_a = list |> Mylist.square()
+    list_b = list |> Enum.map(&(&1 * &1))
+    assert list_a == list_b
   end
 end
