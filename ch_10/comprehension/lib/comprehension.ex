152082acdb70723321da93d1b102e3f322515975
im <poson_hemp@yahoo.co.jp>
2020-07-15 09:34:50 +0900

    add basic function and doctest

--- ch_10/comprehension/lib/comprehension.ex
+++ ch_10/comprehension/lib/comprehension.ex
@@ -1,18 +1,22 @@
 defmodule Comprehension do
   @moduledoc """
-  Documentation for `Comprehension`.
+  内包表記
   """
 
   @doc """
-  Hello world.
+  Function: Comprehension
 
   ## Examples
 
-      iex> Comprehension.hello()
-      :world
+      iex> Comprehension.square([1, 2, 3, 4, 5])
+      [1, 4, 9, 16, 25]
 
   """
   def hello do
     :world
+  end
+
+  def square(arr) do
+    for x <- arr, do: x * x
   end
 end
