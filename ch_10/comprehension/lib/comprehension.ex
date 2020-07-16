ec9fb08cf143a923521e4d57c35abd46768e5517
im <poson_hemp@yahoo.co.jp>
2020-07-16 09:27:22 +0900

    update docs

--- ch_10/comprehension/lib/comprehension.ex
+++ ch_10/comprehension/lib/comprehension.ex
@@ -1,6 +1,6 @@
 defmodule Comprehension do
   @moduledoc """
-  内包表記により、リスト内要素を2乗したリストを返す関数を提供する。
+  内包表記による生成リストを返す関数を提供する。
   """
 
   @doc """
@@ -8,12 +8,14 @@ defmodule Comprehension do
 
   ## Examples
 
+      # リスト内要素を2乗する
       iex> Comprehension.square([1, 2, 3, 4, 5])
       [1, 4, 9, 16, 25]
 
       iex> Comprehension.square_limit_three([1, 2, 3, 4, 5])
       [1, 4, 9]
 
+      # キーワードリストのキーと値を入れ替える
       iex> Comprehension.replace_key_param([kitaq: :clear, fukuoka: :raining, shimonoseki: :smoggy])
       [clear: :kitaq, raining: :fukuoka, smoggy: :shimonoseki]
 
